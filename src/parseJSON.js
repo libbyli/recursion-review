0480fd7e676e951444e4e830630f3bd8c942a0f0
libbyli <sincerelylibby@gmail.com>
2018-04-23 16:28:46 -0700

    Started array and string case for parseJSON.

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,30 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  function renderArray(string) {
+    var array = [];
+    if (string[1] === ']') {
+      return array;
+    }
+    var index = 1;
+    while (index < string.length) {
+      var element = '';
+      while (string[index] !== ',' && index < string.length - 1) {
+        element += string[index];
+        index++;
+      }
+      array.push(parseJSON(element));
+      index++;
+    }
+    return array;
+  }
+  function renderString(string) {
+    return string.slice(1, string.length - 1);
+  }
+  if (json[0] === '[') {
+    return renderArray(json);
+  }
+  if (json[0] === '"') {
+    return renderString(json);
+  }
 };
